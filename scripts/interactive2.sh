cbead9c26226764c32a339b126c6b858eaf99c24
V-Z <vojta@trapa.cz>
2016-01-23 14:43:37 +0100

    New topics, texts

--- scripts/interactive2.sh
+++ scripts/interactive2.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
+
 # Arguments are read from user input (script asks for them)
+
 echo "Please, input first value to sum and press Enter"
+
 read V1
+
 echo "Please, input second value to sum and press Enter"
+
 read V2
+
 echo "Sum of two numbers $V1 and $V2 is `expr $V1 + $V2` ."
+
 echo
+
 exit
