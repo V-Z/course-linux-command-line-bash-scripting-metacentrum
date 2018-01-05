235cb23b9e4848ba15a732bcd8c5fa620c344153
V-Z <vojta@trapa.cz>
2018-01-05 10:55:37 +0100

    Updated index, formatting of scripts.

--- scripts/interactive3.sh
+++ scripts/interactive3.sh
@@ -4,24 +4,24 @@
 # "case" is evaluating provided parameter and behaving accordingly
 
 case "$1" in
-   # "|" means alternatives - more possible inputs
-  -d|--disk)
-    echo "Your disk usage is:"
-    df -h
-    ;;
-  -u|--uptime)
-    echo "Your computer is running:"
-    uptime
-    ;;
-  # This should be every time last possibility - any other input
-  # User is then notified he entered nonsense and gets some help
-  *) # Any other input
-    echo "Wrong option!"
-    echo "Usage: -d or --disk for available disk space or"
-    echo "-u or --uptime for computer uptime"
-    # In this case, exit with error code 1
-    exit 1
-    ;;
-esac
+	# "|" means alternatives - more possible inputs
+	-d|--disk)
+		echo "Your disk usage is:"
+		df -h
+		;;
+	-u|--uptime)
+		echo "Your computer is running:"
+		uptime
+		;;
+	# This should be every time last possibility - any other input
+	# User is then notified he entered nonsense and gets some help
+	*) # Any other input
+		echo "Wrong option!"
+		echo "Usage: -d or --disk for available disk space or"
+		echo "-u or --uptime for computer uptime"
+		# In this case, exit with error code 1
+		exit 1
+		;;
+	esac
 
 exit
--- scripts/interactive4.sh
+++ scripts/interactive4.sh
@@ -7,39 +7,39 @@ NUMBER='^[0-9]+$'
 
 # Function to print help - we will use it twice
 function usagehelp {
-  echo "Usage: number1 plus/minus/product/quotient number2"
-  echo "Use plus for sum, minus for difference, product"
-  echo "for multiplication or quotient for quotient."
-  exit 1 # End up with an error
-  }
+	echo "Usage: number1 plus/minus/product/quotient number2"
+	echo "Use plus for sum, minus for difference, product"
+	echo "for multiplication or quotient for quotient."
+	exit 1 # End up with an error
+	}
 
 # Do we have 3 parameters provided?
 if [ "$#" -ne "3" ]; then
-    echo "Error! Requiring 3 parameters! Received $# ($*)."
-    usagehelp # The function to print help
-  fi
+	echo "Error! Requiring 3 parameters! Received $# ($*)."
+	usagehelp # The function to print help
+	fi
 
 # "=~" means we are testing if $1 fits to regular expression in $NUMBER
 # Is parameter 1 number?
 if [[ ! $1 =~ $NUMBER ]]; then
-    echo "Parameter 1 is not an integer!"
-    usagehelp # The function to print help
-  fi
+	echo "Parameter 1 is not an integer!"
+	usagehelp # The function to print help
+	fi
 
 # Is parameter 3 number?
 if [[ ! $3 =~ $NUMBER ]]; then
-    echo "Parameter 3 is not an integer!"
-    usagehelp # The function to print help
-  fi
+	echo "Parameter 3 is not an integer!"
+	usagehelp # The function to print help
+	fi
 
 case "$2" in
-  plus) expr $1 '+' $3;;
-  minus) expr $1 '-' $3;;
-  product) expr $1 '*' $3;;
-  quotient) expr $1 '/' $3;;
-  *) echo "Wrong option!"
-     usagehelp # The function to print help
-    ;;
-esac
+	plus) expr $1 '+' $3;;
+	minus) expr $1 '-' $3;;
+	product) expr $1 '*' $3;;
+	quotient) expr $1 '/' $3;;
+	*) echo "Wrong option!"
+		usagehelp # The function to print help
+		;;
+	esac
 
 exit
