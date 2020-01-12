ec353e641fd221c02c7eab6949d3f76763d2eb9a
V-Z <vojta@trapa.cz>
2020-01-12 19:20:45 +0100

    Upades of the scripting chapter, added another script example.
    Signed-off-by: V-Z <vojta@trapa.cz>

---
+++ scripts_data/interactive6.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# We expect exactly one parameter
+if [ "$#" -ne "1" ]; then
+	echo "Error! Exactly one parameter is required!"
+	exit 1
+	fi
+
+# Verify that file exists and is readable
+if [ ! -r "$1" ]; then
+	echo "Error! The file provided does not exist or is not readable!"
+	exit 1
+	fi
+
+# Do the operation with input file..
+echo "Size of the file is $(du -sh "$1" | cut -f 1)."
+echo "The file has $(wc -l "$1" | cut -d ' ' -f 1) lines."
+
+echo "Making backup of the file $1..."
+cp "$1" "$1".bak || { echo "Error! Making backup of $1 failed!"; exit 1; }
+
+echo "Done!"
+
+exit
