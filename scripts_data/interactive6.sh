df515d1679a3167e56c2bb0a02f6bb4969d98667
Vojtěch Zeisek <vojta@trapa.cz>
2021-01-24 14:20:06 +0100

    Updated scripting part.

--- scripts_data/interactive6.sh
+++ scripts_data/interactive6.sh
@@ -17,7 +17,8 @@ echo "Size of the file is $(du -sh "$1" | cut -f 1)."
 echo "The file has $(wc -l "$1" | cut -d ' ' -f 1) lines."
 
 echo "Making backup of the file $1..."
-cp "$1" "$1".bak || { echo "Error! Making backup of $1 failed!"; exit 1; }
+# Copy file to backup (*.bak). If it succeeds, report it, if it fails exit with error
+{ cp "$1" "$1".bak && echo "Backup saved as $1".bak; } || { echo "Error! Making backup of $1 failed!"; exit 1; }
 
 echo "Done!"
 
