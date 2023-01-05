0607cc1c481a5db84eae7f9ac8a53a95c1b36954
Vojtěch Zeisek <vojta@trapa.cz>
2023-01-05 16:37:53 +0100

    Updates for 2023. Updated links.

--- web_update.sh
+++ web_update.sh
@@ -5,7 +5,7 @@
 
 echo "Syncing 'linuxcourse' directory"
 echo
-rsync -arv --exclude=*~ --progress linuxcourse laddobar:~/soubory.trapa.cz/htdocs/ || { echo "Error!" && exit 1; }
+rsync -arv --exclude=*~ --progress linuxcourse striga:~/soubory.trapa.cz/htdocs/ || { echo "Error!" && exit 1; }
 echo
 
 # Copy also presentation and scripts and data (pack and Copy)
@@ -13,7 +13,7 @@ if [[ "$1" == "all" ]]; then
 	# Copy presentation
 	echo "Syncing presentation"
 	echo
-	rsync -av --progress presentation/linux_bash_metacentrum_course.pdf laddobar:~/soubory.trapa.cz/htdocs/linuxcourse/ || { echo "Error!" && exit 1; }
+	rsync -av --progress presentation/linux_bash_metacentrum_course.pdf striga:~/soubory.trapa.cz/htdocs/linuxcourse/ || { echo "Error!" && exit 1; }
 	echo
 	# Zip scripts and data
 	echo "Archiving scripts and data"
@@ -23,7 +23,7 @@ if [[ "$1" == "all" ]]; then
 	# Copy the ZIP archive
 	echo "Syncing scripts and data"
 	echo
-	rsync -av --progress scripts_data.zip laddobar:~/soubory.trapa.cz/htdocs/linuxcourse/ || { echo "Error!" && exit 1; }
+	rsync -av --progress scripts_data.zip striga:~/soubory.trapa.cz/htdocs/linuxcourse/ || { echo "Error!" && exit 1; }
 	echo
 	# Delete the local zip archive
 	echo "Removing local archive"
