dbc456a76a6a8660a6b6305ce5dafb8c72320dd6
Vojtěch Zeisek <vojta@trapa.cz>
2024-01-13 15:40:02 +0100

    Small updates of scripting chapter.

--- scripts_data/noninteractive.sh
+++ scripts_data/noninteractive.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-# Simple non-interactive script - no communication with user only list of commands - prints user name, date and $PATH
+# Simple non-interactive script - no communication with user, only list of commands - prints user name, date, $PATH and free space
 
 echo "Hi, ${USER}, today is $(date) and your PATH is ${PATH}."
+
+echo "Free space on your disk:"
+df -h
 
 echo
 
